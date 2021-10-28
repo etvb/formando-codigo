dc79b7f164323633c802e182797d72dc9da89e47
etvb <edgardo.torres@hotmail.com>
2021-10-28 14:24:32 -0500

    finished the menu

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+function show() {
+    // console.log("hol");
+    document.getElementById('sliding_menu').classList.add('open')
+}
+
+function hide() {
+    document.getElementById('sliding_menu').classList.remove('open')
+}
